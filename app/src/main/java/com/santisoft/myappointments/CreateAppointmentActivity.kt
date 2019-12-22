ca74435ea3af0926a251b5d3f47ef4b877508cf1
edwin samir <patino8308@gmail.com>
2019-12-22 13:47:49 +0100

    back top steep 1

--- app/src/main/java/com/santisoft/myappointments/CreateAppointmentActivity.kt
+++ app/src/main/java/com/santisoft/myappointments/CreateAppointmentActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_create_appointment.*
 import java.util.*
 
@@ -21,8 +22,15 @@ class CreateAppointmentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_appointment)
 
         btnNext.setOnClickListener {
-            cvStep1.visibility = View.GONE
-            cvStep2.visibility = View.VISIBLE
+
+            if(etDescription.text.toString().length < 3){
+                etDescription.error = getString(R.string.validate_appoinment_description)
+            }else{
+                cvStep1.visibility = View.GONE
+                cvStep2.visibility = View.VISIBLE
+            }
+
+
         }
 
         btnConfirmAppointment.setOnClickListener {
@@ -99,4 +107,25 @@ class CreateAppointmentActivity : AppCompatActivity() {
     }
 
     private fun Int.twoDigits() = if(this>=10) this.toString() else "0$this"
+
+
+    override fun onBackPressed() {
+        if(cvStep2.visibility == View.VISIBLE){
+            cvStep2.visibility = View.GONE
+            cvStep1.visibility = View.VISIBLE
+
+        }else if(cvStep1.visibility == View.VISIBLE) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.dialog_exit_title))
+            builder.setMessage(getString(R.string.dialog_create_appointment_exit_message))
+            builder.setPositiveButton(getString(R.string.dialog_create_appointment_exit_posotove_button))
+            { _, _ ->
+                finish()
+            }
+            builder.setNegativeButton(getString(R.string.dialog_create_appointment_exit_negative_btn)) { dialog, _ -> dialog.dismiss() }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+    }
 }
